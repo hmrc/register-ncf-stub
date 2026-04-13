fc9bc4fa0151068342d523f02fa7ab516d47ba40
Omiros Trypatsas <omiros.trypatsas@mercator.group>
2026-04-13 13:24:33 +0100

    [RORO-6555][OT]Upgrade bootstrap-play to 10.7.0 and Play sbt-plugin to 3.0.10 (#55)
    * [RORO-6555][OT]Upgrade bootstrap-play to 10.6.0 and Play sbt-plugin to 3.0.10
    
    * [RORO-6555][OT]upgrades bootstrap version
    
    * [RORO-6555][VN]: Undoes the removal of routes from prod.routes
    
    ---------
    
    Co-authored-by: omirostrypatsas <115593155+omirostr1@users.noreply.github.com>
    Co-authored-by: Vedant-N421 <147396311+Vedant-N421@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.19.0"
+  private val bootstrapVersion = "10.7.0"
   private val playVersion      = "play-30"
 
   val compile = Seq(
